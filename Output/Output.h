45db2e7ee0f012a27aa2d7f12b29b12079ca5e1f
Evotopid <evotopid@gmail.com>
2012-08-10 17:19:04 +0200

    updated copyright holder

--- Output/Output.h
+++ Output/Output.h
@@ -1,9 +1,9 @@
 /**
  * @brief An Arduino Library, which helps you to interract with outputs.
- * @author Cheese-Tornado
+ * @author Evotopid
  * 
  * @copyright 
- * Copyright (c) 2011 Cheese-Tornado
+ * Copyright (c) 2011 Evotopid
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
